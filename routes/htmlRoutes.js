417806656608131425fd863933684fe222032fa5
Chris <chrisem816@gmail.com>
2019-04-03 02:02:11 -0700

    init database

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.sellOffer.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
