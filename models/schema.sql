a515abd3ac6dfe218aadb2dfa3322b6e86ff0bf1
C-Swizzle <44960410+C-Swizzle@users.noreply.github.com>
2019-04-02 21:10:46 -0700

    init

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,2 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+DROP DATABASE IF EXISTS tradingapp_db;
+CREATE DATABASE tradingapp_db;
