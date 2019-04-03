8cd78f330006deab9093df990203b71adcd97ab1
Chris <chrisem816@gmail.com>
2019-04-03 02:01:25 -0700

    initialize rough database

---
+++ models/selloffers.js
@@ -0,0 +1,34 @@
+module.exports = function(sequelize, DataTypes) {
+    var sellOffers=sequelize.define("sellOffers",
+    {
+        name:{
+            type:DataTypes.STRING,
+            validation:{
+                notNull:true,
+                notEmpty:true,
+                len:[1,255],
+
+            },
+        price: {
+            type:DataTypes.DECIMAL,
+            validation:{
+                notNull:true,
+                notEmpty:true,
+                min: 0
+            }
+        },
+        description:DataTypes.TEXT,
+        condition:DataTypes.STRING
+        }
+    });
+    sellOffers.associate = function(models) {
+        // saying that sellOffers should belong to Users
+        // A sellOffers can't be created without Users due to the foreign key constraint
+        sellOffers.belongsTo(models.Users, {
+          foreignKey: {
+            allowNull: false
+          }
+        });
+      };
+    return sellOffers;
+}
